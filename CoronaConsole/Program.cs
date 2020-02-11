40b752a788cddcd8eb2acf0e0198eaa6fb9d2e1b
Enrick De Munter <enrick.demunter@student.romerocollege.be>
2020-02-11 09:04:46 +0100

    Add project files.

---
+++ CoronaConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿//Enrick De Munter nr3 5ITN 11/02/2020
+/* opgave
+ * ======
+ * zie blad taak
+ * ANALYSE
+ * =======
+ * VRAAG    
+ * 
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoronaConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DECLARATIES
+            //INPUT
+            //PROCESSING
+            //OUTPUT
+            //WACHTEN
+            Console.WriteLine();
+            Console.WriteLine("druk op enter om af te sluiten!");
+            Console.ReadKey();
+        }
+    }
+}
